453ac89ed412c79bf8d7c2b469a88ece3f5c19bb
adenlall <adenlall@outlook.sa>
2023-07-27 15:40:32 +0100

    v1.8

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,8 +8,8 @@ function Hero() {
                      className="h-full w-full object-cover"
                    />
                    <div className="absolute left-0 right-0 m-auto flex w-2/4 flex-col items-start justify-center gap-4 p-10 backdrop-blur-xl">
-                     <h2 className="text-2xl font-bold text-green-200">Who I'am ?</h2>
-                     <p style={{borederRaduis: "18px"}} className="font-sans text-lg text-green-300">
+                     <h2 className="text-2xl font-extrabold text-green-300">Who I'am ?</h2>
+                     <p style={{"border-raduis": "18px"}} className="font-sans font-bold text-lg text-green-300">
                        Welcome to my public portfolio, I'm aden, full-stack Javascript developer.
                        I used to use React & Next (javascript) for front-end.
                        for the back-end I'm mastering Laravel (php), and I can use Nextjs as full-stack also with expressjs.
